3b883ea36dd6b83225fe00bd1a5aa20c3358247a
zakary1998 <shizexin98@gmail.com>
2019-04-03 23:55:56 +0800

    dao补全

--- src/main/java/com/zakary/services/EventService.java
+++
@@ -1,8 +0,0 @@
-package com.zakary.services;
-import com.zakary.dao.*;
-public interface EventService {
-    int insertDocter(final DoctorDao d);
-    int insertPatient(final PatientDao p);
-    int deleteDoctor(final DoctorDao d);
-    int deletePatient(final PatientDao p);
-}
--- src/test/com/zakary/test/MapperTest.java
+++ src/test/com/zakary/test/MapperTest.java
@@ -1,20 +1,26 @@
 package com.zakary.test;
 
-import com.zakary.dao.DepartmentDao;
-import com.zakary.dao.DoctorDao;
-import com.zakary.dao.PositionDao;
+import com.zakary.dao.*;
+import com.zakary.dao.utils.DoctorPatients;
 import com.zakary.services.DepartmentService;
 import com.zakary.services.DoctorService;
+import com.zakary.services.PatientService;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class MapperTest extends BaseTest {
     @Autowired
     private DepartmentService departmentService;
     @Autowired
     private DoctorService doctorService;
+    @Autowired
+    private PatientService patientService;
     @Test
     public void test1(){
         DepartmentDao departmentDao = new DepartmentDao();
@@ -53,5 +59,29 @@ public class MapperTest extends BaseTest {
             doctorDao.setDoctor_gender("男");
             doctorService.insertDoctor(doctorDao);
         }
+    }
+    @Test
+    public void test6(){
+        String id="2";
+        int pageNum=1;
+        int limit=10;
+        PageDao pageDao = new PageDao();
+        pageDao.setPage((pageNum-1)*limit);
+        pageDao.setLimit(limit);
+        pageDao.setPatient_id(1);
+        List<DoctorPatients> patients=patientService.getAllPatientByDoctorId(pageDao,id);
+
+    }
+    @Test
+    public void test7(){
+        Date date=new Date(2000,1,1);
+        System.out.println(date);
+        TreatmentDao treatmentDao=new TreatmentDao();
+        treatmentDao.setDoctor_id(2);
+        treatmentDao.setPatient_id(5);
+        treatmentDao.setTreatment_name("fff");
+        treatmentDao.setTreatment_time(date);
+        treatmentDao.setTreatment_fee(500.0);
+        patientService.insertPatient(treatmentDao);
     }
 }
