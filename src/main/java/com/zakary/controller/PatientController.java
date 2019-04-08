70135de6479cb471dc5699432e9412b6e2ec2e7c
zakary1998 <shizexin98@gmail.com>
2019-04-08 11:25:56 +0800

    4.8 修改接口

--- src/main/java/com/zakary/controller/PatientController.java
+++ src/main/java/com/zakary/controller/PatientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -22,9 +23,13 @@ public class PatientController {
     private PatientService patientService;
 
     //第一个功能，添加病人在treatment表里，病人必须在patient表里存在
+    //json传patient_cert_code,treatment_time,treatment_name,treatment_fee
     @PostMapping("/doctor_addpatient")
     @ResponseBody
-    public JsonResultDao addPatient(@RequestBody TreatmentDao treatmentDao){
+    public JsonResultDao addPatient(HttpServletRequest request,@RequestBody TreatmentDao treatmentDao){
+        HttpSession session = request.getSession();
+        String cert_code = (String)session.getAttribute("cert_code");
+        treatmentDao.setDoctor_cert_code(cert_code);
         patientService.insertPatient(treatmentDao);
         JsonResultDao jsonResultDao=new JsonResultDao();
         jsonResultDao.setCode(0);
@@ -35,19 +40,22 @@ public class PatientController {
     //第二个功能，查看此医生治疗的所有患者的基本信息,搜索患者
     @RequestMapping("/doctor_patients")
     @ResponseBody
-    public JsonResultDao getPatients(HttpServletRequest request){
+    //json中含有doctor_cert_code，patient_cert_code
+    public JsonResultDao getPatients(HttpServletRequest request,@RequestBody PageDao pageDao){
         int pageNum=Integer.parseInt(request.getParameter("page"));
         int limit=Integer.parseInt(request.getParameter("limit"));
         JsonResultDao jsonResultDao=new JsonResultDao();
-        String doctor_cert_code=request.getParameter("doctor_cert_code");
-        String patient_cert_code=request.getParameter("patient_cert_code");
-        PageDao pageDao = new PageDao();
+        HttpSession session = request.getSession();
+        String cert_code = (String)session.getAttribute("cert_code");
+        //String doctor_cert_code=cert_code;
+        //String patient_cert_code=request.getParameter("patient_cert_code");
+        //PageDao pageDao = new PageDao();
         pageDao.setPage((pageNum-1)*limit);
         pageDao.setLimit(limit);
-        if(patient_cert_code!=null||!patient_cert_code.trim().equals(""))
-            pageDao.setPatient_cert_code(patient_cert_code);
+        //if(pageDao.getPatient_cert_code()!=null||!pageDao.getPatient_cert_code().trim().equals(""))
+        //    pageDao.setPatient_cert_code(pageDao.patient_cert_code);
 
-        List<DoctorPatients> patients=patientService.getAllPatientByDoctorCert(pageDao,doctor_cert_code);
+        List<DoctorPatients> patients=patientService.getAllPatientByDoctorCert(pageDao,cert_code);
         jsonResultDao.setCode(0);
         jsonResultDao.setMsg("success");
         jsonResultDao.setData(patients);
@@ -58,37 +66,39 @@ public class PatientController {
     //安排病房
     @RequestMapping("/ArrangeSickbed")
     @ResponseBody
-    public JsonResultDao arrangeSickbed(HttpServletRequest request){
-        SickbedDao sickbedDao=new SickbedDao();
-        JsonResultDao jsonResultDao=new JsonResultDao();
-        String patient_cert_code=request.getParameter("patient_cert_code");
-        //int cert_code=Integer.parseInt(request.getParameter("cert_code"));
-        int sickroom_id=Integer.parseInt(request.getParameter("sickroom"));
-        int sickbed_id=Integer.parseInt(request.getParameter("sickbed"));
+    //json中含有patient_cert_code，sickroom，sickbed
+    public JsonResultDao arrangeSickbed(HttpServletRequest request,@RequestBody SickbedDao sickbedDao){
+        //SickbedDao sickbedDao=new SickbedDao();
+        //JsonResultDao jsonResultDao=new JsonResultDao();
+        //String patient_cert_code=request.getParameter("patient_cert_code");
+        //int cert_code=Integer.parseInt(request.getParameter("cert_code"))
+        // int sickroom_id=Integer.parseInt(request.getParameter("sickroom"));
+        //int sickbed_id=Integer.parseInt(request.getParameter("sickbed"));
         //sickbedDao.setPatient_id(patient_id);
-        sickbedDao.setPatient_cert_code(patient_cert_code);
-        sickbedDao.setSickroom_id(sickroom_id);
-        sickbedDao.setSickbed_id(sickbed_id);
+        //sickbedDao.setPatient_cert_code(patient_cert_code);
+        //sickbedDao.setSickroom_id(sickroom_id);
+        //sickbedDao.setSickbed_id(sickbed_id);
         sickbedDao.setSickbed_state("full");
 
         patientService.arrangeSickbed(sickbedDao);
 
-        return jsonResultDao;
+        return new JsonResultDao("success");
     }
 
     //病房管理 查询病房信息,搜索患者所属的病房
     @RequestMapping("/GetAllSickbedInfo")
     @ResponseBody
-    public JsonResultDao getAllSickbedInfo(HttpServletRequest request){
+    //json中：如果传入空值则查询所有信息，传入patient_cert_code不为空，则查询当前的病人病床信息
+    public JsonResultDao getAllSickbedInfo(HttpServletRequest request,@RequestBody PageDao pageDao){
         int pageNum=Integer.parseInt(request.getParameter("page"));
         int limit=Integer.parseInt(request.getParameter("limit"));
         JsonResultDao jsonResultDao=new JsonResultDao();
-        String patient_cert_code=request.getParameter("patient_cert_code");
-        PageDao pageDao = new PageDao();
+        //String patient_cert_code=request.getParameter("patient_cert_code");
+        //PageDao pageDao = new PageDao();
         pageDao.setPage((pageNum-1)*limit);
         pageDao.setLimit(limit);
-        if(patient_cert_code!=null||!patient_cert_code.trim().equals(""))//查询患者的病床信息
-            pageDao.setPatient_cert_code(patient_cert_code);
+        //if(patient_cert_code!=null||!patient_cert_code.trim().equals(""))//查询患者的病床信息
+        //    pageDao.setPatient_cert_code(patient_cert_code);
         List<PatientSickbed> patientSickbeds=patientService.getPatientsSickbedInfo(pageDao);
         jsonResultDao.setCode(0);
         jsonResultDao.setMsg("success");
